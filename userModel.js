dc587ae1a33af5a3e3b1524e64783e9b768e5f02
John Blalock <simpledimplejohn@gmail.com>
2023-07-30 09:20:59 -0400

    add userModel.js

---
+++ userModel.js
@@ -0,0 +1,12 @@
+// stores the user data
+class User {
+    constructor(name, birthdate, birthtime, latitude, longitude) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.birthtime = birthtime;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
+
+module.exports = User;
